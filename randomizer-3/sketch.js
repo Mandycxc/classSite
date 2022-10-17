baa12ef49fc00da6195a19b0de673861591b1aeb
Mandycxc <mandyc02008@gmail.com>
2022-10-17 14:39:41 -0700

    update

--- randomizer-3/sketch.js
+++ randomizer-3/sketch.js
@@ -1,59 +1,71 @@
 
-let dogs = [{
-  name: "dilion",
-  color: "dog treat"
+let things = [{
+  event: "Watch Scary Movie",
+  condition: "at 12 am"
 }, {
-  name: "Ziggy",
-  color: "chartrese"
+  event: "Order Food",
+  condition: "that's not on the menu"
 }, {
-  name: "mercedes",
-  color: "poop"
+  event: "Shout Out",
+  condition: " 'I am an idiot' in public"
 }, {
-  name: "petunia",
-  color: "rose gold"
+  event: "Do Push Up",
+  condition: "with one thinger"
 }, {
-  name: "kloot",
-  color: "coffee grind brown"
+  event: "House Cleaning",
+  condition: "for a month"
 }, {
-  name: "nala",
-  color: "stardust"
+  event: "Take out the trash",
+  condition: "for two weeks"
 }]
 
+
+
 let randomIndex;
-let animating = true;
+let animating = false;
+let meme = [];
+
+function preload(){
+  for (let i = 0; i <= 15; i++){
+    trolls[i] = loadImage(`Assets/meme_${i}.JPG`)
+  }
+}
 
 function setup(){
 	createCanvas(600, 600);
 	background(200);
   textSize(32);
 
-text("click to randomize", 50, 50);
-
-
-
+text("click here to accept your faith", 50, 50);
+console.log(trolls);
 }
+
+
 function draw(){
 
-if(animating == true){
+if (animating == true){
+    fill(128, 0,0);
+strokeWeight(1);
   ellipse(random(width),random(height), random(50,200));
+
 }
+
 }
 function randomizer(){
-    if (dogs[0]){
-    background(random(200,255));
-    randomIndex = int(random(dogs.length));
-   text(dogs[randomIndex].name, 50, 50);
-  dogs.splice(randomIndex, 1);
+  animating = false;
+    if (things[0]){
+    randomIndex = int(random(things.length));
+   text(`${things[randomIndex].event}
+    ${things[randomIndex].condition}`, 50, 50);
+    // text(things[randomIndex].event + "'s favortute condition is " + things[randomIndex].condition, 50, 50);
+  things.splice(randomIndex, 1);
   }else{
     background(random(200,255));
     text("nothing left!", 50, 50);
   }
 }
 function mousePressed(){
-
+animating = true;
 setTimeout(randomizer,2000);
-
-
-
 
 }
